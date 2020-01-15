05c22e39ca15b232654d7e334b6021bdf97ed72d
WhitePeachTea <penglei2123@gmail.com>
2020-01-14 19:22:45 -0500

    .env created

---
+++ resultsbygeolocation.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv').config();
+var zomato = require('zomato-api');
+const api = process.env.apiKey
+var client = zomato({
+    userKey: api
+})
+
+client.getGeocode({lat: 49.267941, lon: -123.247360})
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
