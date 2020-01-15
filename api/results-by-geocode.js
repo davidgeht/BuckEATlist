7c2ea4e1960c3325ab0eafd5f97611e20395bcd8
WhitePeachTea <penglei2123@gmail.com>
2020-01-14 21:30:52 -0500

    api update

--- api/results-by-geocode.js
+++ api/results-by-geocode.js
@@ -1,10 +1,10 @@
 const dotenv = require('dotenv').config();
 var zomato = require('zomato-api');
-const api = process.env.apiKey
+const api = process.env.apiKey;
 var client = zomato({
     userKey: api
-})
+});
 
 client.getGeocode({lat: 49.267941, lon: -123.247360})
-  .then(res => console.log(res))
+  .then(res => console.log(res.nearby_restaurants))
   .catch(err => console.log(err));
