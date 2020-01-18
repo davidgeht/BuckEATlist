ccc6830ab2c407675df533840172bdd81bfe569c
Michael Hrivnak <mhrivnak@gmail.com>
2020-01-18 17:13:42 -0500

    added search results basic formatting

--- public/js/search.js
+++ public/js/search.js
@@ -15,8 +15,33 @@ $("#searchForm").submit( event => {
     };
     
     $.post('/api/search/restaurantsnearby',body)
-    .then(function(data){        
-    
+    .then(function(data){
+        renderResults(data);
     });     
    
 });
+
+function renderResults(data){
+    let contentStr = "<h3>Search Results</h3>\n";
+    
+    if(data.length === 0){
+        return `${contentStr}
+        <span>0 results found</span>`;
+    }
+
+    for(const restaurant of data){        
+        contentStr += `<div class="restaurantContrainer row align-items-center">\n
+        <span class="restName">${restaurant.name}</span>\n           
+        <button class="addToList" data-restaurant='${JSON.stringify(restaurant)}' data-yelpid="${restaurant.id}"><i class="fas fa-plus"></i></button>\n            
+        </div>\n`;
+    }
+    
+    $("#results").html(contentStr);
+
+    $(".addToList").on('click', function(event){                
+        let restaurant = $(event.currentTarget).data("restaurant");        
+        $.post(`/api/buckeatlist/add`, restaurant)
+        .then();            
+    });
+    
+}
